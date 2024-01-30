25324042c23c1e904c19ae1e8597acea28faed45
aritradey-CS <aritra1999.dey@gmail.com>
2024-01-30 20:37:37 +0530

    Comparison Operator - Greater then or equal

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ const numOne = 2;
 const numTwo = 1;
 
 console.log(numOne <= numTwo);
+// -------------------------------------------
+const numOnee = 3;
+const numtwoo = 2;
+const numThree = 2;
+
+console.log(numOnee > numtwoo, numtwoo > numThree);
+
+
+
+// Comparison Operator - Greater then or equal
