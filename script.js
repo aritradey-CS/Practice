88aa42c8346b0d1b634a16f3af731dd1d4400164
aritradey-CS <aritra1999.dey@gmail.com>
2024-02-01 20:26:28 +0530

    Comparison Operator

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const priceTwo = 10;
 
 console.log(priceOne < priceTwo);
 console.log(priceTwo < priceOne);
+// -------------------------------------------
+
+const numOne = 2;
+const numTwo = 1;
+
+console.log(numOne <= numTwo);
