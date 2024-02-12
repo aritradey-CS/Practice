8e2ed7d77729f62ec2bfd183d0b0103aca40aae5
aritradey-CS <aritra1999.dey@gmail.com>
2024-02-12 22:11:19 +0530

    Merge Sorted Array

---
+++ script2.js
@@ -0,0 +1,41 @@
+// You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+// Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+// The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+function mergeSortedArray(nums1, m, nums2, n) {
+    let i = m - 1; // Pointer for nums1
+    let j = n - 1; // Pointer for nums2
+    let k = m + n - 1; // Pointer for the merged array (nums1)
+
+    // Merge from the end of the arrays
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] > nums2[j]) {
+            nums1[k] = nums1[i];
+            i--;
+        } else {
+            nums1[k] = nums2[j];
+            j--;
+        }
+        k--;
+    }
+
+    // If there are remaining elements in nums2, copy them to nums1
+    while (j >= 0) {
+        nums1[k] = nums2[j];
+        j--;
+        k--;
+    }
+}
+
+// Example usage:
+const nums1 = [1, 2, 3, 0, 0, 0]; // Initial nums1 array
+const m = 3; // Number of elements in nums1
+const nums2 = [2, 5, 6]; // nums2 array
+const n = 3; // Number of elements in nums2
+
+mergeSortedArray(nums1, m, nums2, n);
+
+// The merged array is now stored in nums1
+console.log(nums1); // Output: [1, 2, 2, 3, 5, 6]
